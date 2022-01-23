cb6438274bcf1f1d5b984d80e34f799c4c00c7e3
Myron Turner <turnermm02@shaw.ca>
2022-01-23 08:29:36 -0600

    Reformat code cli.php, helper.php

--- cli.php
+++ cli.php
@@ -1,136 +1,145 @@
 <?php
-if(!defined('DOKU_INC')) die();
+if (!defined('DOKU_INC')) die();
+
 use splitbrain\phpcli\Options;
- 
-class cli_plugin_newpagetemplate extends DokuWiki_CLI_Plugin {
+
+class cli_plugin_newpagetemplate extends DokuWiki_CLI_Plugin
+{
     // register options and arguments
     public $newpgdbg = false;
-    protected function setup(Options $options) {
-       // $this->colors->disable();
-        $config =  $this->colors->wrap('config','cyan') ;
+
+    protected function setup(Options $options)
+    {
+        // $this->colors->disable();
+        $config = $this->colors->wrap('config', 'cyan');
 
         $options->setHelp('This plugin helps to automate the processing of pages that use new page templates. ' .
             'The first command line option must be either page or ini.  For a complete description see ' .
             'the newpagetemplate documentation at https://www.dokuwiki.org/plugin:newpagetemplate');
         $options->registerOption('version', 'print version', 'v');
-        $options->registerOption('page', 'Apply the template to the named page id', 'p');  
-        $options->registerOption('usrrepl', 'Macro/Replacent string: @MACRO@,replacement;@MACRO_2@. . . ', 'u');           
-        $options->registerOption('tmpl', 'Template to apply to the specified page ', 't');   
+        $options->registerOption('page', 'Apply the template to the named page id', 'p');
+        $options->registerOption('usrrepl', 'Macro/Replacent string: @MACRO@,replacement;@MACRO_2@. . . ', 'u');
+        $options->registerOption('tmpl', 'Template to apply to the specified page ', 't');
         $options->registerOption('owner', 'User/owner of current process', 'o');
         $options->registerOption('ini', "Name of alternate ini file. This file must be stored in the root directory" .
-        " of the newpagetemplate plugin. Its format is described on the newpagetemplate" .
-        " plugin page. To use the ini file specified in the plugin's Configuration Settings, this option must be set to $config.", 'i');        
-        
-    }
-    
-    protected function main(Options $options) {
-        print_r($options->getArgs(),1); 
+            " of the newpagetemplate plugin. Its format is described on the newpagetemplate" .
+            " plugin page. To use the ini file specified in the plugin's Configuration Settings, this option must be set to $config.", 'i');
 
-        if($options->getOpt('page')) {          
-            $opts = $options->getArgs();         
-            $clopts = $this->get_commandLineOptions($opts);       
-            if(!$clopts['page']) {
+    }
+
+    protected function main(Options $options)
+    {
+        print_r($options->getArgs(), 1);
+
+        if ($options->getOpt('page')) {
+            $opts = $options->getArgs();
+            $clopts = $this->get_commandLineOptions($opts);
+            if (!$clopts['page']) {
                 $this->commandLineErr('page');
             }
-            $this->raw_commandLineOpts($opts,$clopts); 
-            $helper = plugin_load('helper','newpagetemplate',1,$false);            
-            $helper->init($clopts,$options,$this);
-        } 
-        else if ($options->getOpt('ini')) {
-            $opts = $options->getArgs();           
-            $clopts = $this->get_commandLineOptions($opts,1);
-            if(!$clopts['ini']) {
+            $this->raw_commandLineOpts($opts, $clopts);
+            $helper = plugin_load('helper', 'newpagetemplate', 1, $false);
+            $helper->init($clopts, $options, $this);
+        } else if ($options->getOpt('ini')) {
+            $opts = $options->getArgs();
+            $clopts = $this->get_commandLineOptions($opts, 1);
+            if (!$clopts['ini']) {
                 $this->commandLineErr('ini');
-            }        
-            $this->raw_commandLineOpts($opts,$clopts);                  
-            $helper = plugin_load('helper','newpagetemplate',1,$false);
-            $helper->init($clopts,$options,$this);
-        }
-        else if ($options->getOpt('version')) {
-            $info = $this->getInfo();    
+            }
+            $this->raw_commandLineOpts($opts, $clopts);
+            $helper = plugin_load('helper', 'newpagetemplate', 1, $false);
+            $helper->init($clopts, $options, $this);
+        } else if ($options->getOpt('version')) {
+            $info = $this->getInfo();
             $this->success($info['date']);
-        }  else {
+        } else {
             echo $options->help();
         }
 
     }
-    
-    function get_commandLineOptions($opts,$config = false) {
-        if(function_exists('is_countable') &&!is_countable($opts)) return;
-      
-        $page=""; $usrrepl="";$user = "";$ini = false;
-        if($config) {
-           $ini = array_shift($opts);          
-        }
-        else $page = array_shift($opts);
-        for($i=0; $i<count($opts); $i++) {
-            $cl_switch = trim($opts[$i],'-');
-            switch ($cl_switch) { // leaves space for additional command line options
-            case 'u':
-            case 'usrrepl':
-                $usrrepl =  $opts[$i+1];         
-                break;
-            case 't':
-            case 'utsrrepl':
-                $tmpl =  $opts[$i+1];         
-                break;        
-            case 'o':
-            case 'owner':
-               $user =  $opts[$i+1];         
-                break;
-            case 'cliuser':
-            case 'c':
-               $user = $opts[$i+1];
-               break; 
-            }
-        }
-        if(!$user) {
-            $processUser = posix_getpwuid(posix_geteuid());
-            if(isset($processUser) && !empty($processUser['name'])) {
-                $user =  $processUser['name'];
-            }
-        }
-                   
-          return array('page'=>$page, 'usrrepl'=>$usrrepl,'tmpl'=>$tmpl, 'user'=>$user, 'ini'=>$ini);
-        }      
 
-    public function raw_commandLineOpts($opts="",$clopts="", $dbg=false) { 
-        if(!$this->newpgdbg && !$dbg) return;    
-      global $argv;
-        echo "argv = " .  print_r($argv,1);
-        print_r($opts);
-        print_r($clopts);       
+    function get_commandLineOptions($opts, $config = false)
+    {
+        if (function_exists('is_countable') && !is_countable($opts)) return;
+
+        $page = "";
+        $usrrepl = "";
+        $user = "";
+        $ini = false;
+        if ($config) {
+            $ini = array_shift($opts);
+        } else $page = array_shift($opts);
+        for ($i = 0; $i < count($opts); $i++) {
+            $cl_switch = trim($opts[$i], '-');
+            switch ($cl_switch) { // leaves space for additional command line options
+                case 'u':
+                case 'usrrepl':
+                    $usrrepl = $opts[$i + 1];
+                    break;
+                case 't':
+                case 'utsrrepl':
+                    $tmpl = $opts[$i + 1];
+                    break;
+                case 'o':
+                case 'owner':
+                    $user = $opts[$i + 1];
+                    break;
+                case 'cliuser':
+                case 'c':
+                    $user = $opts[$i + 1];
+                    break;
+            }
+        }
+        if (!$user) {
+            $processUser = posix_getpwuid(posix_geteuid());
+            if (isset($processUser) && !empty($processUser['name'])) {
+                $user = $processUser['name'];
+            }
+        }
+
+        return array('page' => $page, 'usrrepl' => $usrrepl, 'tmpl' => $tmpl, 'user' => $user, 'ini' => $ini);
     }
-    
-    function commandLineErr($type) {
-        switch($type) {
-           case 'page':
-               $this->colors->ptln("$type error: page id required",'cyan');
-               exit;              
-           case 'ini':
-               $this->colors->ptln("$type error: ini configuration file required",'cyan');
-               exit; 
-            default:   
-         }
-       return 1;
-    }       
-/**
- * @param array      $array
- * @param int|string $position
- * @param mixed      $insert
- */
+
+    public function raw_commandLineOpts($opts = "", $clopts = "", $dbg = false)
+    {
+        if (!$this->newpgdbg && !$dbg) return;
+        global $argv;
+        echo "argv = " . print_r($argv, 1);
+        print_r($opts);
+        print_r($clopts);
+    }
+
+    function commandLineErr($type)
+    {
+        switch ($type) {
+            case 'page':
+                $this->colors->ptln("$type error: page id required", 'cyan');
+                exit;
+            case 'ini':
+                $this->colors->ptln("$type error: ini configuration file required", 'cyan');
+                exit;
+            default:
+        }
+        return 1;
+    }
+
+    /**
+     * @param array $array
+     * @param int|string $position
+     * @param mixed $insert
+     */
     function array_insert(&$array, $position, $insert)
     {
         if (is_int($position)) {
             array_splice($array, $position, 0, $insert);
         } else {
-            $pos   = array_search($position, array_keys($array));
+            $pos = array_search($position, array_keys($array));
             $array = array_merge(
                 array_slice($array, 0, $pos),
                 $insert,
                 array_slice($array, $pos)
             );
         }
-    }        
-              
+    }
+
 }
--- helper.php
+++ helper.php
@@ -1,171 +1,177 @@
 <?php
-if(!defined('DOKU_INC')) die();
+if (!defined('DOKU_INC')) die();
 
-class helper_plugin_newpagetemplate extends DokuWiki_Plugin {
+class helper_plugin_newpagetemplate extends DokuWiki_Plugin
+{
     public $template;
     private $debug = false;
-    function init($opts,$options,$cli) {  
-        $page=$opts['page'];
-        $usrreplace=$opts['usrrepl'];
-        $template=$opts['tmpl'];
-        $overwrite=$opts['overwr'];
-        $ini=$opts['ini'];
+
+    function init($opts, $options, $cli)
+    {
+        $page = $opts['page'];
+        $usrreplace = $opts['usrrepl'];
+        $template = $opts['tmpl'];
+        $overwrite = $opts['overwr'];
+        $ini = $opts['ini'];
         $user = $opts['user'];
-        $ini = $opts['ini']; 
-        
-        if($this->debug) {
-            $cli_opts = $options->getArgs();       
-            $cli->raw_commandLineOpts($cli_opts,$opts,1);
+        $ini = $opts['ini'];
+
+        if ($this->debug) {
+            $cli_opts = $options->getArgs();
+            $cli->raw_commandLineOpts($cli_opts, $opts, 1);
             return;
         }
 
-        if(!empty($template)) {
+        if (!empty($template)) {
             $template = wikiFN($template);
             echo "Template: $template \n";
-            $tpl = $this->pagefromtemplate($opts['tmpl'],$opts['page'],$opts['usrrepl'],$opts['user']);
-           //$this->writePage($opts['page'],$tpl);
-            echo $tpl . "\n";        
-        }   
-        else if(isset($opts['ini'])) {
-             $this->output_ini($user,$ini);
+            $tpl = $this->pagefromtemplate($opts['tmpl'], $opts['page'], $opts['usrrepl'], $opts['user']);
+            //$this->writePage($opts['page'],$tpl);
+            echo $tpl . "\n";
+        } else if (isset($opts['ini'])) {
+            $this->output_ini($user, $ini);
         }
-       //$this->writePage($page,$tpl);
-    
+        //$this->writePage($page,$tpl);
 
-     }
-    
-    function writePage($page,$tpl) {
-       $file = wikiFn($page);   
-       io_saveFile($file, $tpl, false);
-       chown($file, 'www-data');       
-       chgrp($file, 'www-data');
-       chmod($file, 0664);
+
     }
 
-    function pagefromtemplate($template,$page,$newpagevars,$user = "") {      
-     
-          global $conf;
-          global $INFO;
-          $ID = $page;
-        
-          $tpl = io_readFile(wikiFN($template));
-     
-          if($this->getConf('userreplace')) {
-            $stringvars =
-                array_map(function($v) { return explode(",",$v,2);}, explode(';',$newpagevars)); 
-            foreach($stringvars as $value) {
-                 $tpl = str_replace(trim($value[0]),hsc(trim($value[1])),$tpl);
-            }
-         }
+    function writePage($page, $tpl)
+    {
+        $file = wikiFn($page);
+        io_saveFile($file, $tpl, false);
+        chown($file, 'www-data');
+        chgrp($file, 'www-data');
+        chmod($file, 0664);
+    }
 
-          if($this->getConf('standardreplace')) {           
-            $file = noNS($ID);       
-            $page = cleanID($file) ;
-            if($this->getConf('prettytitles')) {        
-                $title= str_replace('_',' ',$page);
+    function pagefromtemplate($template, $page, $newpagevars, $user = "")
+    {
+
+        global $conf;
+        global $INFO;
+        $ID = $page;
+
+        $tpl = io_readFile(wikiFN($template));
+
+        if ($this->getConf('userreplace')) {
+            $stringvars =
+                array_map(function ($v) {
+                    return explode(",", $v, 2);
+                }, explode(';', $newpagevars));
+            foreach ($stringvars as $value) {
+                $tpl = str_replace(trim($value[0]), hsc(trim($value[1])), $tpl);
             }
-           else {
-               $title = $page;
-           }
-             if(class_exists('\dokuwiki\\Utf8\PhpString')) {
+        }
+
+        if ($this->getConf('standardreplace')) {
+            $file = noNS($ID);
+            $page = cleanID($file);
+            if ($this->getConf('prettytitles')) {
+                $title = str_replace('_', ' ', $page);
+            } else {
+                $title = $page;
+            }
+            if (class_exists('\dokuwiki\\Utf8\PhpString')) {
                 $ucfirst = '\dokuwiki\Utf8\PhpString::ucfirst';
                 $ucwords = '\dokuwiki\\Utf8\PhpString::ucwords';
                 $ucupper = '\dokuwiki\\Utf8\PhpString::strtoupper';
-              
-            }
-           else {
+
+            } else {
                 $ucfirst = 'utf8_ucfirst';
                 $ucwords = 'utf8_ucwords';
                 $ucupper = 'utf8_strtoupper';
-               
-            }    
-            
-            $tpl = str_replace(array(
-                                  '@ID@',
-                                  '@NS@',
-                                  '@CURNS@',
-                                  '@!CURNS@',
-                                  '@!!CURNS@',
-                                  '@!CURNS!@',
-                                  '@FILE@',
-                                  '@!FILE@',
-                                  '@!FILE!@',
-                                  '@PAGE@',
-                                  '@!PAGE@',
-                                  '@!!PAGE@',
-                                  '@!PAGE!@',
-                                  '@USER@',
-                                  '@DATE@',
-                                  '@EVENT@'
-                               ),
-                               array(
-                                  $ID,
-                                  getNS($ID),
-                                  curNS($ID),
-                                  $ucfirst(curNS($ID)),
-                                  $ucwords(curNS($ID)),
-                                  $ucupper(curNS($ID)),
-                                  $file,
-                                  $ucfirst($file),
-                                  $ucupper($file),
-                                  $page,
-                                  $ucfirst($title),
-                                  $ucwords($title),
-                                  $ucupper($title),
-                                  $user,
-                                  $conf['dformat'],
-                                  $event->name ,
-                               ), $tpl);
-     
-            // we need the callback to work around strftime's char limit
-            $tpl = preg_replace_callback('/%./',create_function('$m','return strftime($m[0]);'),$tpl);
-          }
-          if($this->getConf('skip_unset_macros')) {
-              $tpl = preg_replace("/@.*?@/ms","",$tpl);
-          }
-          return $tpl;
-      }
 
-    function output_ini($opts,$ini) {
-        if($ini == 'config') {
+            }
+
+            $tpl = str_replace(array(
+                '@ID@',
+                '@NS@',
+                '@CURNS@',
+                '@!CURNS@',
+                '@!!CURNS@',
+                '@!CURNS!@',
+                '@FILE@',
+                '@!FILE@',
+                '@!FILE!@',
+                '@PAGE@',
+                '@!PAGE@',
+                '@!!PAGE@',
+                '@!PAGE!@',
+                '@USER@',
+                '@DATE@',
+                '@EVENT@'
+            ),
+                array(
+                    $ID,
+                    getNS($ID),
+                    curNS($ID),
+                    $ucfirst(curNS($ID)),
+                    $ucwords(curNS($ID)),
+                    $ucupper(curNS($ID)),
+                    $file,
+                    $ucfirst($file),
+                    $ucupper($file),
+                    $page,
+                    $ucfirst($title),
+                    $ucwords($title),
+                    $ucupper($title),
+                    $user,
+                    $conf['dformat'],
+                    $event->name,
+                ), $tpl);
+
+            // we need the callback to work around strftime's char limit
+            $tpl = preg_replace_callback('/%./', create_function('$m', 'return strftime($m[0]);'), $tpl);
+        }
+        if ($this->getConf('skip_unset_macros')) {
+            $tpl = preg_replace("/@.*?@/ms", "", $tpl);
+        }
+        return $tpl;
+    }
+
+    function output_ini($opts, $ini)
+    {
+        if ($ini == 'config') {
             $ini_file = $this->getConf('default_ini');
-            $ini_file = DOKU_INC ."lib/plugins/newpagetemplate/$ini_file";
-           // echo "$ini_file\n"; return;
+            $ini_file = DOKU_INC . "lib/plugins/newpagetemplate/$ini_file";
+            // echo "$ini_file\n"; return;
+        } else {
+            $ini_file = DOKU_INC . "lib/plugins/newpagetemplate/$ini";
         }
-        else {
-            $ini_file = DOKU_INC ."lib/plugins/newpagetemplate/$ini";
-        }
-       // echo "Ini File: $ini_file\n"; 
-        $ini = parse_ini_file($ini_file,1);
+        // echo "Ini File: $ini_file\n";
+        $ini = parse_ini_file($ini_file, 1);
         $templ = array_keys($ini);
-        foreach($templ AS $t) {           
+        foreach ($templ as $t) {
             $pages = $ini[$t]['page'];
             $newpagevars = $ini[$t]['newpagevars'];
-            if(is_array($newpagevars)) {
+            if (is_array($newpagevars)) {
                 echo "\n$t\n\n";
-                $this->process_array($pages,$newpagevars, $t,$user);
-            }
-            else { 
+                $this->process_array($pages, $newpagevars, $t, $user);
+            } else {
                 echo "\n$t\n\n";
-                $this->process_single($pages,$newpagevars, $t,$user);
+                $this->process_single($pages, $newpagevars, $t, $user);
             }
         }
     }
-    function process_array($pages,$newpagevars, $tpl,$user) {       
-          for($i = 0; $i < count($pages); $i++) {
-              $res = $this->pagefromtemplate($tpl,$pages[$i],$newpagevars[$i],$user); 
-              echo "Output: ". "\n" . $res . "\n";
-              echo "\n===================\n";
-          }  
 
-      }
-      
-    function process_single($pages,$newpagevars, $tpl,$user) {  
-          for($i = 0; $i < count($pages); $i++) {
-              $res = $this->pagefromtemplate($tpl,$pages[$i],$newpagevars,$user); 
-              echo "Output: ". "\n" .$res . "\n";
-              echo "\n===================\n";
-          }
-      }
+    function process_array($pages, $newpagevars, $tpl, $user)
+    {
+        for ($i = 0; $i < count($pages); $i++) {
+            $res = $this->pagefromtemplate($tpl, $pages[$i], $newpagevars[$i], $user);
+            echo "Output: " . "\n" . $res . "\n";
+            echo "\n===================\n";
+        }
+
+    }
+
+    function process_single($pages, $newpagevars, $tpl, $user)
+    {
+        for ($i = 0; $i < count($pages); $i++) {
+            $res = $this->pagefromtemplate($tpl, $pages[$i], $newpagevars, $user);
+            echo "Output: " . "\n" . $res . "\n";
+            echo "\n===================\n";
+        }
+    }
 
 }
