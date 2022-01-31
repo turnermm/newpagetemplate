60b9fee65871f0256727fb59ba2dec69f093b78f
Myron Turner <turnermm02@shaw.ca>
2022-01-31 08:41:51 -0600

    Adds metadata and default entries for server owner and group

--- conf/default.php
+++ conf/default.php
@@ -6,4 +6,5 @@ $conf['prettytitles']=0;
 $conf['skip_unset_macros']=0;
 $conf['default_ini'] = "";
 $conf['default_tplns'] = "pagetemplates";
-
+$conf['web_owner']  = 'www-data';
+$conf['web_group']  = 'www-data';
--- conf/metadata.php
+++ conf/metadata.php
@@ -5,4 +5,9 @@ $meta['prettytitles'] = array('onoff');
 $meta['skip_unset_macros'] = array('onoff');
 $meta['default_ini'] = array('string');
 $meta['default_tplns'] = array('string');
+$meta['web_owner']  = array('multicheckbox','_choices' => array('www-data','apache'));
+$meta['web_group']  = array('multicheckbox','_choices' => array('www-data','apache'));
+
+
+
 
