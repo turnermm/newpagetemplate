32049560a63121b3a2dd23ddbc9f8c7e91861f54
Damien Regad <dregad@mantisbt.org>
2023-10-19 15:06:19 +0200

    Fix deprecated warnings
    Remove unnecessary require_once calls.
    
    Fixes #11

--- action.php
+++ action.php
@@ -17,8 +17,6 @@
 if(!defined('DOKU_INC')) die();
 
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'action.php');
-require_once(DOKU_INC.'inc/init.php');
 
 class action_plugin_newpagetemplate extends DokuWiki_Action_Plugin {
    var $done = false;
